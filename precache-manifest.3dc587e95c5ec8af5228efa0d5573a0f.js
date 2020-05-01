3ca12c5e31d5653834207c2bbc93131cbf6d7db6
ardaatahan <63500745+ardaatahan@users.noreply.github.com>
2020-05-01 22:47:40 +0300

    Updates

---
+++ precache-manifest.3dc587e95c5ec8af5228efa0d5573a0f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b96d66e9755bfadc20ade3ddb802b8f1",
+    "url": "/react-corona-tracker/index.html"
+  },
+  {
+    "revision": "6123c5f96f43f1de17c9",
+    "url": "/react-corona-tracker/static/css/main.3c98f865.chunk.css"
+  },
+  {
+    "revision": "b1f4f048f34dbe8f9008",
+    "url": "/react-corona-tracker/static/js/2.1ac96ede.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/react-corona-tracker/static/js/2.1ac96ede.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6123c5f96f43f1de17c9",
+    "url": "/react-corona-tracker/static/js/main.dfac9965.chunk.js"
+  },
+  {
+    "revision": "99f37624b83980ed2aba",
+    "url": "/react-corona-tracker/static/js/runtime-main.44c93f30.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/react-corona-tracker/static/media/coronaimage.d7265326.png"
+  }
+]);
